e981a2b06da1b04df7f5828e2002ab6562c21069
oscyaldamysha <oscaraldamasha@gmail.com>
2018-10-30 11:23:54 -0700

    10/30/18

--- jump man/behavior-hx/SceneEvents_6.hx
+++ jump man/behavior-hx/SceneEvents_6.hx
@@ -40,6 +40,7 @@ import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Body;
 import box2D.dynamics.B2Fixture;
 import box2D.dynamics.joints.B2Joint;
+import box2D.collision.shapes.B2Shape;
 
 import motion.Actuate;
 import motion.easing.Back;
@@ -69,13 +70,13 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class ActorEvents_29 extends ActorScript
+class SceneEvents_6 extends SceneScript
 {
 	
 	
-	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	public function new(dummy:Int, dummy2:Engine)
 	{
-		super(actor);
+		super();
 		
 	}
 	
